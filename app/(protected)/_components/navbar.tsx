044bbd3917197ce2a9c238eb5392e04cdc3a2a8f
BradFernando <bfcorro@espe.edu.ec>
2024-05-13 21:06:30 -0500

    Commit de arreglos del nav responsivo

--- app/(protected)/_components/navbar.tsx
+++ app/(protected)/_components/navbar.tsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
   const pathname = usePathname();
 
   return (
-    <nav className="bg-secondary flex justify-between items-center p-4 rounded-xl w-[600px] shadow-sm">
+    <nav className="bg-secondary flex justify-between items-center p-4 rounded-xl w-full md:w-[600px] shadow-sm">
       <div className="flex gap-x-2">
         <Button asChild variant={pathname === "/server" ? "default" : "outline"}>
           <Link href="/server">
